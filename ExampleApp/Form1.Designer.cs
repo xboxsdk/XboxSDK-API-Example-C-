f3c30fa1691f595541df63353b788c716c9058dc
Joseph Newing <jnewing@gmail.com>
2012-05-23 19:36:03 -0400

    Example C# Application

---
+++ ExampleApp/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ExampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.apikey = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dlid = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "API Key";
+            // 
+            // apikey
+            // 
+            this.apikey.Location = new System.Drawing.Point(57, 23);
+            this.apikey.Name = "apikey";
+            this.apikey.Size = new System.Drawing.Size(298, 20);
+            this.apikey.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.apikey);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 59);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "API Key";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get User Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(408, 212);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Get Profiles";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(375, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(196, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Download";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dlid
+            // 
+            this.dlid.Location = new System.Drawing.Point(277, 79);
+            this.dlid.Name = "dlid";
+            this.dlid.Size = new System.Drawing.Size(32, 20);
+            this.dlid.TabIndex = 8;
+            this.dlid.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 332);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(408, 48);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(361, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(40, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 387);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dlid);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "XboxSDK API Example C# Application 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox apikey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox dlid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ExampleApp/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+// =========================================================================================================================================
+//                      bbbbbbbb                                                                                                          
+// XXXXXXX       XXXXXXXb::::::b                                                  SSSSSSSSSSSSSSS DDDDDDDDDDDDD       KKKKKKKKK    KKKKKKK
+// X:::::X       X:::::Xb::::::b                                                SS:::::::::::::::SD::::::::::::DDD    K:::::::K    K:::::K
+// X:::::X       X:::::Xb::::::b                                               S:::::SSSSSS::::::SD:::::::::::::::DD  K:::::::K    K:::::K
+// X::::::X     X::::::X b:::::b                                               S:::::S     SSSSSSSDDD:::::DDDDD:::::D K:::::::K   K::::::K
+// XXX:::::X   X:::::XXX b:::::bbbbbbbbb       ooooooooooo xxxxxxx      xxxxxxxS:::::S              D:::::D    D:::::DKK::::::K  K:::::KKK
+//    X:::::X X:::::X    b::::::::::::::bb   oo:::::::::::oox:::::x    x:::::x S:::::S              D:::::D     D:::::D K:::::K K:::::K   
+//     X:::::X:::::X     b::::::::::::::::b o:::::::::::::::ox:::::x  x:::::x   S::::SSSS           D:::::D     D:::::D K::::::K:::::K    
+//      X:::::::::X      b:::::bbbbb:::::::bo:::::ooooo:::::o x:::::xx:::::x     SS::::::SSSSS      D:::::D     D:::::D K:::::::::::K     
+//      X:::::::::X      b:::::b    b::::::bo::::o     o::::o  x::::::::::x        SSS::::::::SS    D:::::D     D:::::D K:::::::::::K     
+//     X:::::X:::::X     b:::::b     b:::::bo::::o     o::::o   x::::::::x            SSSSSS::::S   D:::::D     D:::::D K::::::K:::::K    
+//    X:::::X X:::::X    b:::::b     b:::::bo::::o     o::::o   x::::::::x                 S:::::S  D:::::D     D:::::D K:::::K K:::::K   
+// XXX:::::X   X:::::XXX b:::::b     b:::::bo::::o     o::::o  x::::::::::x                S:::::S  D:::::D    D:::::DKK::::::K  K:::::KKK
+// X::::::X     X::::::X b:::::bbbbbb::::::bo:::::ooooo:::::o x:::::xx:::::x   SSSSSSS     S:::::SDDD:::::DDDDD:::::D K:::::::K   K::::::K
+// X:::::X       X:::::X b::::::::::::::::b o:::::::::::::::ox:::::x  x:::::x  S::::::SSSSSS:::::SD:::::::::::::::DD  K:::::::K    K:::::K
+// X:::::X       X:::::X b:::::::::::::::b   oo:::::::::::oox:::::x    x:::::x S:::::::::::::::SS D::::::::::::DDD    K:::::::K    K:::::K
+// XXXXXXX       XXXXXXX bbbbbbbbbbbbbbbb      ooooooooooo xxxxxxx      xxxxxxx SSSSSSSSSSSSSSS   DDDDDDDDDDDDD       KKKKKKKKK    KKKKKKK
+// =========================================================================================================================================
+// https://xboxsdk.com & https://xboxapi.com
+
+/**
+ * In this example we used Newtonsoft's JSON Parsing libarary.
+ * This can be found: http://james.newtonking.com/projects/json-net.aspx
+ * Thanks to James Newton-King for this great libaray.
+ */
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+
+namespace ExampleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (apikey.Text == "" || apikey.Text == null)
+            {
+                MessageBox.Show("Error: I need an API Key");
+                return;
+            }
+            
+            // our api url
+            String url = "http://savelab.net/v2/index.php/api/user/" + apikey.Text;
+
+            // build our api request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // setup some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+
+            // setup credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            // read our API response
+            string json_data = readStream.ReadToEnd();
+
+            // deserialize our json data into a nice JObject (thanks Newtonsoft <3)
+            JObject user_info = JObject.Parse(json_data);
+
+            listBox1.Items.Add("------------------------ JSON RESPONSE ----------------");
+            listBox1.Items.Add("JSON Data: " + json_data);
+            listBox1.Items.Add("------------------------ END RESPONSE ----------------");
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add("------------------------ Formatted Data ----------------");
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add("Username: " + user_info["user_name"]);
+            listBox1.Items.Add("Email: " + user_info["user_email"]);
+            listBox1.Items.Add("Alias: " + user_info["user_alias"]);
+
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add(" ");
+
+            response.Close();
+            readStream.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (apikey.Text == "" || apikey.Text == null)
+            {
+                MessageBox.Show("Error: I need an API Key");
+                return;
+            }
+
+            // our api url
+            String url = "http://savelab.net/v2/index.php/api/profiles/" + apikey.Text;
+
+            // build our api request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // setup some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+
+            // setup credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            // read our API response
+            string json_data = readStream.ReadToEnd();
+
+            // deserialize our json data into a nice JObject (thanks Newtonsoft <3)
+            JArray user_profiles = JArray.Parse(json_data);
+            //JObject user_profiles = JObject.Parse(json_data);
+
+            listBox1.Items.Add("------------------------ JSON RESPONSE ----------------");
+            listBox1.Items.Add("JSON Data: " + json_data);
+            listBox1.Items.Add("------------------------ END RESPONSE ----------------");
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add("------------------------ Formatted Data ----------------");
+            listBox1.Items.Add(" ");
+
+            foreach (JObject profile in user_profiles)
+            {
+                listBox1.Items.Add("---------------- Profile [ " + profile["id"] + " ]  ----------------");
+                listBox1.Items.Add("Profile Name: " + profile["profile_name"]);
+                listBox1.Items.Add("Profile ID: " + profile["profile_id"]);
+                listBox1.Items.Add(" ");
+            }
+
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add(" ");
+
+            response.Close();
+            readStream.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (apikey.Text == "" || apikey.Text == null)
+            {
+                MessageBox.Show("Error: I need an API Key");
+                return;
+            }
+
+            // our api url
+            String url = "http://savelab.net/v2/index.php/api/download/" + apikey.Text + "/" + dlid.Text;
+
+            // build our api request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // setup some reasonable limits on resources used by this request
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+
+            // setup credentials to use for this request.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // listbox output
+            listBox1.Items.Add("Downloading Save ID: 1...");
+
+            // get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            // read our API response
+            string json_data = readStream.ReadToEnd();
+
+            // listbox output
+            listBox1.Items.Add("Save Downloaded!");
+
+            // deserialize our json data into a nice JObject (thanks Newtonsoft <3)
+            JObject user_download = JObject.Parse(json_data);
+
+            // get our real filename
+            string file_name = (string)user_download["filename"];
+
+            // listbox output
+            listBox1.Items.Add("Writing out save to: " + Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + file_name);
+            
+            // get our save file data as a base64 encoded string
+            byte[] fileData = Convert.FromBase64String((string)user_download["data"]);
+
+            // write ouRRRRRR byte[] array to rrRRRRR save, says pirate pete.
+            System.IO.File.WriteAllBytes( Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + file_name, fileData );
+
+            MessageBox.Show("Finsihed!");
+
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add(" ");
+
+            response.Close();
+            readStream.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Environment.CurrentDirectory + @"\api.key"))
+                apikey.Text = File.ReadAllText(Environment.CurrentDirectory + @"\api.key");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(Environment.CurrentDirectory + @"\api.key", apikey.Text);
+        }
+    }
+    
+}
